4ffec0830921af23d41b45cf2dfe25ed290e2201
carsnwd <carsnwd@gmail.com>
2016-09-08 23:35:25 -0400

    Finished assignment 3

---
+++ core/src/com/woodgdx/game/util/Constants.java
@@ -0,0 +1,13 @@
+package com.woodgdx.game.util;
+
+/**
+ * To keep track of constant variables.
+ * @author carson
+ */
+
+public class Constants {
+	// Visible game world is 5 meters wide
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	// Visible game world is 5 meters tall
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+}
--- core/src/com/woodgdx/game/woodGdxGame.java
+++ core/src/com/woodgdx/game/woodGdxGame.java
@@ -1,33 +1,57 @@
 package com.woodgdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class woodGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class woodGdxGame implements ApplicationListener {
+	private static final String TAG =
+			woodGdxGame.class.getName();
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		//Set LIbGDX log level to DEBUG so it prints to console.
+		Gdx.app.setLogLevel(Application.LOG_DEBUG); //LOG_NONE cancels it.
+		//Initialize controler and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);		
 	}
-
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void resize(int width, int height) {
+		worldRenderer.resize(width, height);
+		
+	}
+	@Override
+	public void render() {
+		//Update game world by the time that has passed
+		//since last rendered frame.
+		worldController.update(Gdx.graphics.getDeltaTime());
+		//Sets the clear screen color to Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f,
+				0xff/255.0f);
+		//Clears the screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		//Render game world to screen
+		worldRenderer.render();
 	}
-	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void dispose() {
+		worldRenderer.dispose();
 	}
 }
